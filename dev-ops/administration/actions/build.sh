6a01b835a2ca281feb30ff97c9475b588441c9d1
Jannik Jochem <jannik.jochem@pickware.com>
2019-08-09 16:03:34 +0200

    Skip bundle:dump and assets:install steps

--- dev-ops/administration/actions/build.sh
+++ dev-ops/administration/actions/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #DESCRIPTION: build administration for production and run assetic
 
-bin/console bundle:dump
+# bin/console bundle:dump
 PROJECT_ROOT=__PROJECT_ROOT__ npm run --prefix vendor/shopware/platform/src/Administration/Resources/administration/ build
-bin/console assets:install
+# bin/console assets:install
